b43cdd1b13ee9bf8ae941d5be1bbed80b2751e4d
Raul Rodriguez Proenza <rproenza86@gmail.com>
2018-01-10 21:57:03 -0500

    feat: cached static assets

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,25 @@
-addEventListener('fetch', event => {
+const CACHE_NAME = 'wittr-static-v1';
+const urlsToCache = [
+    '/',
+    'js/main.js',
+    'css/main.css',
+    'imgs/icon.png',
+    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+];
+
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            console.info('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
     // Let the browser do its default thing
     // for non-GET requests.
     if (event.request.method != 'GET') return;
