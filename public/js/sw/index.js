49dbb3721c4f56c293f3a044677ad3d4a5b1e26f
Raul Rodriguez Proenza <rproenza86@gmail.com>
2018-01-10 22:23:55 -0500

    feat: return cached assets before fetch

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -19,20 +19,33 @@ self.addEventListener('install', event => {
     );
 });
 
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        // TODO: remove the old cache
+    );
+});
+
 self.addEventListener('fetch', event => {
     // Let the browser do its default thing
     // for non-GET requests.
     if (event.request.method != 'GET') return;
 
     event.respondWith(
-        fetch(event.request).then(response => {
-            if (response.status === 404) {
-                return fetch('/imgs/dr-evil.gif');
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
             }
-            return response;
-        }).catch(err => {
-            console.error(err);
-            return new Response("Resource request failed");
+            return fetch(event.request).then(response => {
+                if (response.status === 404) {
+                    return fetch('/imgs/dr-evil.gif');
+                }
+                return response;
+            }).catch(err => {
+                console.error(err);
+                return new Response("Resource request failed");
+            });
         })
     );
 });
